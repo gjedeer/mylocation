3891db7f830fe10ee4793127972b7c902cea9288
mypapit <mypapit@da9f8531-91b2-6cc8-ea17-d202e1f035eb>
2012-08-17 12:19:16 +0000

    updated license information, GNU GPL 2.0

--- trunk/src/net/mypapit/mobile/myposition/AboutDialog.java
+++ trunk/src/net/mypapit/mobile/myposition/AboutDialog.java
@@ -1,5 +1,33 @@
 package net.mypapit.mobile.myposition;
 
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * MyLocation 1.1c for Android <mypapit@gmail.com> (9w2wtf)
+ * Copyright 2012 Mohammad Hafiz bin Ismail. All rights reserved.
+ *
+ * Info url :
+ * http://code.google.com/p/mylocation/
+ * http://kirostudio.com
+ * http://blog.mypapit.net/
+ * 
+ * 
+ * AboutDialog.java
+ * Custom class for displaying About Dialog
+ * My GPS Location Tool
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
--- trunk/src/net/mypapit/mobile/myposition/ConverterActivity.java
+++ trunk/src/net/mypapit/mobile/myposition/ConverterActivity.java
@@ -1,5 +1,31 @@
 package net.mypapit.mobile.myposition;
-
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * MyLocation 1.1c for Android <mypapit@gmail.com> (9w2wtf)
+ * Copyright 2012 Mohammad Hafiz bin Ismail. All rights reserved.
+ *
+ * Info url :
+ * http://code.google.com/p/mylocation/
+ * http://kirostudio.com
+ * http://blog.mypapit.net/
+ * 
+ * 
+ * ConverterActivity.java
+ * Tool for converting between Decimal GPS coordinate and DD MM SS coordinate
+ * My GPS Location Tool
+ */
 import java.text.DecimalFormat;
 import java.util.StringTokenizer;
 
