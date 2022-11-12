cc9adc88f9946cc237300989434b3e2abb819c78
GDR! <gdr@gdr.name>
2022-11-12 11:45:58 +0100

    Change gmaps URL up to spec
    Changed URL according to specification here:
    https://developers.google.com/maps/documentation/urls/get-started

--- app/src/main/java/net/mypapit/mobile/myposition/MyLocationActivity.java
+++ app/src/main/java/net/mypapit/mobile/myposition/MyLocationActivity.java
@@ -215,7 +215,7 @@ public class MyLocationActivity extends Activity implements OnClickListener, Loc
 		message.append("\nhttps://openstreetmap.org/go/");
 		message.append(MapUtils.createShortLinkString(lat, lon, 15));
 		message.append("?m");
-		message.append("\n\nhttps://maps.google.com/maps?q=loc:" + lat + "," + lon + "&z=15");
+		message.append("\n\nhttps://maps.google.com/maps/search/?api=1&query=" + lat + "%2C" + lon);
 		message.append("\n\nhttp://download.osmand.net/go?lat=" + lat + "&lon=" + lon + "&z=15");
 		message.append("\n\ngeo:");
 		message.append(Float.toString((float)lat));
