a21a06442abf8f2e1ebca3dccc32e86ccee114eb
Ansgar Esztermann <aeszter@gwdg.de>
2023-05-19 12:07:08 +0200

    fix spurious import

--- app/src/main/java/net/mypapit/mobile/myposition/MyLocationActivity.java
+++ app/src/main/java/net/mypapit/mobile/myposition/MyLocationActivity.java
@@ -58,7 +58,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.preference.PreferenceManager;
-import android.support.annotation.MainThread;
 import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.Menu;
